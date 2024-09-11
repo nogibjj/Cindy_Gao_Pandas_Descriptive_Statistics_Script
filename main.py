702ade58c58436580e49d0f20f5dd9cbe740d0e6
Cindy Gao <Cindy_ggjx@bunanadangongju.lan>
2024-09-10 20:48:44 -0400

    add pandas files

--- main.py
+++ main.py
@@ -1,6 +1,69 @@
-def add(x, y):
-    return x + y
+"""Requirements:
+
+Python script using Pandas for descriptive statistics
+Read a dataset (CSV or Excel)
+Generate summary statistics (mean, median, standard deviation)
+Create at least one data visualization
+Deliverable:
+
+Python script
+Generated summary report (PDF or markdown)"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
 
-def subtract(x, y):
-    return x - y
+df = pd.read_csv(
+    "https://raw.githubusercontent.com/anlane611/datasets/main/population.csv"
+)
+
+df_y = df["Y"]
+
+
+# calculate the mean of variable Y:
+def get_mean(script):
+    sum = 0
+    for i in range(len(script)):
+        sum += script[i]
+
+    mean_script = sum / len(script)
+    return round(mean_script, 3)
+
+
+print(get_mean(df_y))
+
+
+# calculate the median of variable Y:
+def get_median(script):
+    sorted_script = np.sort(script)
+    num = len(sorted_script)
+    if num % 2 == 0:
+        median_script = (sorted_script[num // 2] + sorted_script[num // 2 + 1]) / 2
+    else:
+        median_script = sorted_script[np.ceil(num / 2)]
+    return round(median_script, 3)
+
+
+print(get_median(df_y))
+
+
+def get_std(script):
+    num = len(script)
+    mean_s = np.mean(script)
+    sum_error = 0
+    for i in range(num):
+        sum_error += (mean_s - script[i]) ** 2
+    std_script = np.sqrt(sum_error / (num - 1))
+    return round(std_script, 3)
+
+
+print(get_std(df_y))
+
+
+# data visualization: boxplot for variabnle Y"
+plt.boxplot(df_y)
+plt.xlabel("variable_Y")
+plt.ylabel("values")
+plt.title("Visualization for Boxplot of variable_Y")
+plt.show()
--- test_main.py
+++ test_main.py
@@ -1,16 +1,30 @@
-from main import add, subtract
+from main import get_mean, get_median, get_std
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv(
+    "https://raw.githubusercontent.com/anlane611/datasets/main/population.csv"
+)
+
+df_y = df["Y"]
 
 
-def test_add():
-    """testing out add function"""
-    assert add(2, 2) == 4
-    assert add(1, 3) == 4
+def test_mean():
+    """testing out get_mean function"""
+    assert get_mean(df_y) == round(np.mean(df_y), 3)
 
 
-def test_subtract():
-    assert subtract(9, 3) == 6
-    assert subtract(27, 10) == 17
+def test_median():
+    """testing out get_median function"""
+    assert get_median(df_y) == round(np.median(df_y), 3)
+
+
+def test_std():
+    """testing out get_std function"""
+    assert get_std(df_y) == round(np.std(df_y), 3)
 
 
 if __name__ == "__main__":
-    test_add()
+    test_mean()
+    test_median()
+    test_std()
