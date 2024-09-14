99aadc55b1efea43a3fb055edc49dbc0821d1aad
Cindy Gao <Cindy_ggjx@bunanadangongju.lan>
2024-09-14 14:55:43 -0400

    modify files

--- test_main.py
+++ test_main.py
@@ -5,36 +5,42 @@ import pandas as pd
 
 data = "https://raw.githubusercontent.com/anlane611/datasets/main/population.csv"
 dataframe = load_dataset(data)
-df_y = dataframe["Y"]
 
-print(df_y)
+
+print(dataframe)
 
 # Print descriptive statistics
-print(df_y.describe())
+print(dataframe.describe())
+print(get_mean(dataframe["Y"]))
+print(get_median(dataframe["Y"]))
+print(get_std(dataframe["Y"]))
 
 
 # Define test functions
 def test_mean():
     """Test the get_mean function"""
-    assert get_mean(df_y) == round(np.mean(df_y), 3)
+    assert get_mean(dataframe["Y"]) == round(np.mean(dataframe["Y"]), 3)
+    assert get_mean(dataframe["X1"]) == round(np.mean(dataframe["X1"]), 3)
 
 
 def test_median():
     """Test the get_median function"""
-    assert get_median(df_y) == round(np.median(df_y), 3)
+    assert get_median(dataframe["Y"]) == round(np.median(dataframe["Y"]), 3)
+    assert get_median(dataframe["X1"]) == round(np.median(dataframe["X1"]), 3)
 
 
 def test_std():
     """Test the get_std function"""
-    assert get_std(df_y) == round(np.std(df_y), 3)
+    assert get_std(dataframe["Y"]) == round(np.std(dataframe["Y"]), 3)
+    assert get_std(dataframe["X2"]) == round(np.std(dataframe["X2"]), 3)
 
 
 if __name__ == "__main__":
     test_mean()
     test_median()
     test_std()
-    create_boxplot(df_y, "boxplot.png")
-    mean_y = get_mean(df_y)
-    median_y = get_median(df_y)
-    std_y = get_std(df_y)
+    create_boxplot(dataframe["Y"], "boxplot.png")
+    mean_y = get_mean(dataframe["Y"])
+    median_y = get_median(dataframe["Y"])
+    std_y = get_std(dataframe["Y"])
     save_to_md(mean_y, median_y, std_y)
